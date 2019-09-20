5b3ff8ba01d40e0533e875fdc7496f8566049bee
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-09-20 23:22:53 +0100

    I1: recording ideas

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
+require './idea'
+
 class IdeaBoxApp < Sinatra::Base
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  not_found do
+    erb :error
+  end
+
   get '/' do
-    "Hello, World!"
+    erb :index
+  end
+
+  post '/' do
+    idea = Idea.new
+    idea.save
+    "Creating an IDEA!"
   end
 end
