160865b93fd00208398f9dbc5bbb67e62ccc5602
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-09-22 23:31:34 +0100

    Refactor Ideas and create Idea Store

--- app.rb
+++ app.rb
@@ -1,18 +1,14 @@
 require './idea'
-
+require './idea_store'
 class IdeaBoxApp < Sinatra::Base
   set :method_override, true
-
-  configure :development do
-    register Sinatra::Reloader
-  end
 
   not_found do
     erb :error
   end
 
   get '/' do
-    erb :index, locals: {ideas: Idea.all}
+    erb :index, locals: {ideas: IdeaStore.all, idea: Idea.new(params)}
   end
 
   post '/' do
@@ -22,17 +18,17 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   delete '/:id' do |id|
-    Idea.delete(id)
+    IdeaStore.delete(id.to_i)
     redirect '/'
   end
 
   get '/:id/edit' do |id|
-    idea = Idea.find(id)
+    idea = IdeaStore.find(id.to_i)
     erb :edit, locals: {id: id, idea: idea}
   end
 
   put '/:id' do |id|
-    Idea.update(id.to_i, params[:idea])
+    IdeaStore.update(id.to_i, params[:idea])
     redirect '/'
   end
 end
--- idea.rb
+++ idea.rb
@@ -1,57 +1,7 @@
-require 'yaml/store'
 class Idea
   attr_reader :title, :description
   def initialize(attributes)
     @title = attributes["title"]
     @description = attributes["description"]
-  end
-
-  def self.all
-    raw_ideas.map do |data|
-      Idea.new(data)
-    end
-  end
-  
-  def self.raw_ideas
-    database.transaction do |db|
-      db['ideas'] || []
-    end
-  end
-  
-  def save
-    database.transaction do |db|
-      db['ideas'] ||= []
-      db['ideas'] << {"title" => @title, "description" => @description}
-    end
-  end
-
-  def self.delete(position)
-    database.transaction do
-      database['ideas'].delete_at(position.to_i)
-    end
-  end
-  
-  def self.find(id)
-    Idea.new(find_raw_idea(id))
-  end
-  
-  def self.find_raw_idea(id)
-    database.transaction do
-      database['ideas'].at(id.to_i)
-    end
-  end
-
-  def self.update(id, data)
-    database.transaction do
-      database['ideas'][id] = data
-    end
-  end
-
-  def self.database
-    @database ||= YAML::Store.new "ideabox"
-  end
-
-  def database
-    Idea.database
   end
 end
