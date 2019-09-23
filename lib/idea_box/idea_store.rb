d622f9c7df5f4d58edc09fe86dab31e7f0cd208a
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-09-23 13:31:50 +0100

    I9: Add ranking and sorting

--- lib/idea_box/idea_store.rb
+++ lib/idea_box/idea_store.rb
@@ -1,20 +1,22 @@
 require 'yaml/store'
 class IdeaStore
   def self.all
-    raw_ideas.map do |data|
-      Idea.new(data)
+    ideas = []
+    raw_ideas.each_with_index do |data, i|
+      ideas << Idea.new(data.merge("id" => i))
     end
+    ideas
   end
 
-  def self.create(attributes)
+  def self.create(data)
     database.transaction do
-      database['ideas'] ||= []
-      database['ideas'] << attributes
+      database['ideas'] << data
     end
   end
   
   def self.find(id)
-    Idea.new(find_raw_idea(id))
+    raw_idea = find_raw_idea(id)
+    Idea.new(raw_idea.merge("id" => id))
   end
   
   def self.update(id, data)
@@ -42,6 +44,11 @@ class IdeaStore
   end
   
   def self.database
-    @database ||= YAML::Store.new "db/ideabox"
+    return @database if @database
+    @database = YAML::Store.new('db/ideabox')
+    @database.transaction do
+      @database['ideas'] ||= []
+    end
+    @database
   end
 end
